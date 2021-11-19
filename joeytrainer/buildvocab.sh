03563c369752f45eb3f2ba3c04d12b3b4cf324c2
BenAji <ayoadebenjamin@gmail.com>
2021-11-19 17:13:29 +0100

    update vocab.sh

--- joeytrainer/buildvocab.sh
+++ joeytrainer/buildvocab.sh
@@ -2,6 +2,6 @@ src=fr
 tgt=ee
 #mkdir -p joeynmt/data/frmos/fr_mos/10k/
 
-python joeynmt/scripts/build_vocab.py /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/10k/JW/train.$src /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/10k/JW/train.$tgt --output_path /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/10k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/10k/JW/train.$src /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/10k/JW/train.$tgt --output_path /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/10k/JW/vocab.txt
 
-python joeynmt/scripts/build_vocab.py /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/20k/JW/train.$src /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/20k/JW/train.$tgt --output_path /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/20k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/20k/JW/train.$src /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/20k/JW/train.$tgt --output_path /content/gdrive/MyDrive/Masakhane/lafand/lafand-mt/joeytrainer/data/free/spdata/20k/JW/vocab.txt
